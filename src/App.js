310358da2680fa26c57bf329c74baa0a9af2aeb0
herearunsharma <herearunsharma@gmail.com>
2023-04-07 12:11:22 +0530

    Theme
    Light and Dark Theme added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { Routes, Route } from "react-router-dom";
+import Sticky from "./components/Sticky";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Navbar />
+
+      <Routes>
+        <Route path="/AllTools" element={<AllTools />} />
+        <Route path="/" exact element={<Hero />} />
+      </Routes>
+
+      <Footer /> */}
+      <Sticky />
     </div>
   );
-}
+};
 
 export default App;
